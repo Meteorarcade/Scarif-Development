99e861780bb62767c71215e88b6e028ad0fd5bf4
Meteorarcade <connorhpowell@gmail.com>
2026-07-28 14:50:32 +1000

    2

--- IoT/src/comms.h
+++ IoT/src/comms.h
@@ -1,5 +1,3 @@
-
-
 /*
  * Communications logic for the development module, including MQTT setup, 
  * message handling, and periodic updates.
@@ -17,7 +15,7 @@ PubSubClient client(espClient);
 String topicBuffer;
 
 // MQTT Broker configuration (Default MQTT port is 1883)
-const char* mqttServer = "192.168.68.104";  
+const char* mqttServer = "192.168.1.116";  
 const int mqttPort = 1883;
 
 unsigned long lastUpdate = 0;
